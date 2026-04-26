18bf0a550f72fefd1ab7411dc8f344d158c9c9ef
yessinekoubaa2002-svg <yessinekoubaa@yahoo.fr>
2026-04-26 15:24:10 +0100

    personnecreated

---
+++ src/main/java/com/example/biblio/model/Personne.java
@@ -0,0 +1,60 @@
+package com.example.biblio.model;
+
+public class Personne {
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+
+    // constructeur
+    public Personne(String nom, String prenom, String email, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+    }
+
+    // getters & setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
